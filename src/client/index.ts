81b69ed44150478d12ac5dfb28698d8092335f10
Dogbane <dogbane.dev@gmail.com>
2025-12-13 09:38:18 -0500

    add age verified cookie

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,6 +4,9 @@ import { remixParseHandler, remixRedirectHandler } from './middleware'
 
 const client: Client<paths, `${string}/${string}`> = createClient<paths>({
 	baseUrl: 'https://www.clips4sale.com',
+	headers: {
+		Cookie: 'ageVerified=true;',
+	},
 })
 
 client.use(remixParseHandler, remixRedirectHandler)
