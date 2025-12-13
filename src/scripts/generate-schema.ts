e828664c12ea900c43284d2e50bde9ea78433e8b
Dogbane <dogbane.dev@gmail.com>
2025-12-13 15:58:15 -0500

    swagger attempt

--- src/scripts/generate-schema.ts
+++
@@ -1,9 +0,0 @@
-import openapiTS, { astToString, type OpenAPI3 } from 'openapi-typescript'
-import { schema } from '../open-api/schema'
-
-// open api schema json
-await Bun.file('./public/schema.json').write(JSON.stringify(schema, null, 2))
-
-// open api auto generated types
-const contents = astToString(await openapiTS(schema as OpenAPI3))
-await Bun.file('./src/open-api/paths.generated.ts').write(contents)
